f61ef8bb31d3797ddbf27ddef0ede197c896656c
Mohsen <solimohsen94@gmail.com>
2024-06-04 16:23:03 +0330

    Fix imports

--- app/src/main/java/com/mohsen/composeplayground/home/HomeScreen.kt
+++ app/src/main/java/com/mohsen/composeplayground/home/HomeScreen.kt
@@ -7,7 +7,7 @@ import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.unit.dp
-import com.mohsen.composeplayground.LocalNavController
+import navigation.LocalNavController
 import navigation.Route
 
 @Composable
